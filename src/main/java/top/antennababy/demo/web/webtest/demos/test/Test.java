b76a89de4e8f1aa1c18c0b4518d67d0e3a359660
lixiangyang <18116874+Antenbabby@users.noreply.github.com>
2024-08-15 14:49:42 +0800

    webSocket 多实例测试.

--- src/main/java/top/antennababy/demo/web/webtest/demos/test/Test.java
+++ src/main/java/top/antennababy/demo/web/webtest/demos/test/Test.java
@@ -2,15 +2,22 @@ package top.antennababy.demo.web.webtest.demos.test;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import lombok.SneakyThrows;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.concurrent.Future;
 
 public class Test {
+    @SneakyThrows
     public static void main(String[] args) {
         String format = DateUtil.format(DateUtil.offsetMinute(DateUtil.beginOfDay(new Date())
                 , Integer.parseInt(DateUtil.between(DateUtil.beginOfDay(new Date())
                         , DateUtil.parse("2024-08-12 12:57", "yyyy-MM-dd HH:mm"), DateUnit.MINUTE) / 5 * 5 + "")), "HH:mm");
-        System.out.printf("活跃时间占比:"+format );
+        System.out.println("活跃时间占比:"+format );
+        Future<String> stringFuture = ThreadUtil.execAsync(() -> "11111111");
+        System.out.println(stringFuture.get());
+        System.out.println(stringFuture.get());
     }
 }
